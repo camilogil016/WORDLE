209f380bb64effbb07133e6b2a27cc88aa73fc26
acamg16 <andres.gils@usantotomas.edu.co>
2022-03-06 15:57:24 -0500

    Se sigue trabajando en el teclado

--- src/interfaz.js
+++ src/interfaz.js
@@ -1,29 +1,58 @@
-const chalk = require("chalk");
+chalk = require("chalk");
 
 function teclado(palabras) {
-  let letras = "QWERTYUIOPASDFGHJKLÑ >ZXCVBNM";
+  let letras = [
+    "Q",
+    "W",
+    "E",
+    "R",
+    "T",
+    "Y",
+    "U",
+    "I",
+    "O",
+    "P",
+    "A",
+    "S",
+    "D",
+    "F",
+    "G",
+    "H",
+    "J",
+    "K",
+    "L",
+    "Ñ",
+    " ",
+    "Z",
+    "X",
+    "C",
+    "V",
+    "B",
+    "N",
+    "M",
+  ];
   let parte = "";
-  for (let i = 0; i < palabras.length; i++){
-    for(let j = 0; j < palabras[i].length; j++){
-      for(k in letras){
-        if(k == palabras[i][j][0]){
-          if(palabras[i][j][1] == 0) {
-            letras[k]= chalk.bgWhite(`${palabras[i][j][0]}`);
-          } else if(palabras[i][j][1] == 1) {
-            letras[k]= chalk.bgYellow(`${palabras[i][j][0]}`);
-          } else if(palabras[i][j][1] == 2) {
-            letras[k]= chalk.bgGreen(`${palabras[i][j][0]}`);
-          }
-          console.log(letras[k]);
-        }else{
-          letras[k] = chalk.bgGray(k);
-        }        
+  for (let i = 0; i < palabras.length; i++) {
+    for (let j = 0; j < palabras[i].length; j++) {
+      if (letras.some((x) => x == palabras[i][j][0])) {
+        let indice = letras.indexOf(palabras[i][j][1]);
+        if (palabras[i][j][1] == 0) {
+          letras[indice] = chalk.bgWhite(`${palabras[i][j][0]}`);
+          console.log("No existe");
+        } else if (palabras[i][j][1] == 1) {
+          letras[indice] = chalk.bgYellow(`${palabras[i][j][0]}`);
+          console.log("Esta mal");
+        } else if (palabras[i][j][1] == 2) {
+          letras[indice] = chalk.bgGreen(`${palabras[i][j][0]}`);
+          console.log("Esta nice");
+        }
       }
     }
   }
-  console.log(letras);
+
   for (i in letras) {
     if (letras[i] != " ") {
+      parte += letras[i] + " ";
       if (letras[i] === "P" || letras[i] === "Ñ" || letras[i] === "M") {
         console.log(parte);
         parte = "";
@@ -35,19 +64,20 @@ function teclado(palabras) {
 }
 
 async function pantalla(palabras) {
-  for(let i=0; i<palabras.length ;i++) {
+  for (let i = 0; i < palabras.length; i++) {
     console.log(chalk.cyan("\t-----------------------"));
     let pared = chalk.cyan(" | ");
     process.stdout.write("\t");
-    for(let j=0; j<palabras[i].length;j++) {
+    for (let j = 0; j < palabras[i].length; j++) {
       process.stdout.write(pared);
-      if(palabras[i][j][1] == -1) {
+      if (palabras[i][j][1] == -1) {
         process.stdout.write(chalk.white(`${palabras[i][j][0]}`));
-      } if(palabras[i][j][1] == 0) {
+      }
+      if (palabras[i][j][1] == 0) {
         process.stdout.write(chalk.gray(`${palabras[i][j][0]}`));
-      } else if(palabras[i][j][1] == 1) {
+      } else if (palabras[i][j][1] == 1) {
         process.stdout.write(chalk.yellow(`${palabras[i][j][0]}`));
-      } else if(palabras[i][j][1] == 2) {
+      } else if (palabras[i][j][1] == 2) {
         process.stdout.write(chalk.green(`${palabras[i][j][0]}`));
       }
     }
@@ -58,7 +88,7 @@ async function pantalla(palabras) {
 }
 
 function imprimir(palabras) {
-  console.clear();  
+  console.clear();
   pantalla(palabras);
   console.log("");
   teclado(palabras);
@@ -66,5 +96,5 @@ function imprimir(palabras) {
 }
 
 module.exports = {
-  imprimir
-}
+  imprimir,
+};
