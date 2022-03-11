b2705c3c68df4359f88951ff02e3521170573b97
sebcalle <sebastianmeza@usantotomas.edu.co>
2022-03-10 21:54:27 -0500

    Se comenta menuJuego.js

--- src/menuJuego.js
+++ src/menuJuego.js
@@ -2,22 +2,23 @@ const {menuJugador} = require("./input.js")
 const {imprimirEstadisticas} = require("./estadisticas.js")
 const {juego} = require("./juego.js")
 
+//Funcion del menu, con entrada del username para luego guardar e imprimir sus estadisticas
 async function menuJuego(username) {
-    seguir = true;
+    seguir = true; //boolean que sirve para preguntar si el usuario quiere seguir jugando o no.
     while(seguir) {
         //console.clear();
         let opcion = await menuJugador();
         switch(opcion.numero) {
             case 1:
-                await juego(username);
+                await juego(username);  //Lleva la opcion al juego de Wordle
                 break;
 
             case 2:
-                imprimirEstadisticas(username);
+                imprimirEstadisticas(username); //Leva a la funcion de imprimir las estadisticas del usuario
                 break;
             
             case 3:
-                seguir = false;
+                seguir = false; //Por si el usuario quiere salirse del juego
                 break;
             default:
                 break;
