8b32fa29e59defa96d0ef7ce1ead0727a596b225
sebcalle <sebastianmeza@usantotomas.edu.co>
2022-03-10 21:58:04 -0500

    Se comenta las funciones de wordle.js

--- wordle.js
+++ wordle.js
@@ -3,28 +3,29 @@ const { juego } = require("./src/juego.js");
 const { menuJuego } = require("./src/menuJuego");
 const { iniciarSesion, crearCuenta } = require("./src/cuentas.js");
 
+//Funcion principal del juego que se encarga de tener el menu para crear cuenta, iniciar sesion o salirse del juego.
 async function main() {
   console.clear();
-  let seguir = true;
+  let seguir = true;  //Bolean para saber si el usuario desea salirse
   while (seguir) {
     let opcion = await menu();
     switch (opcion.numero) {
       case 1:
         while (true) {
           let verificar = await iniciarSesion();
-          if (verificar[0]) {
+          if (verificar[0]) { //En caso de que el usuario y contraseña sean correctos.
             console.clear();
             await menuJuego(verificar[1]);
             break;
             false;
-          } else {
+          } else {    //En caso de que el usuario y contraseña sean incorrectos
             console.log("El usuario y contraseña no son correctos.");
           }
         }
         break;
 
       case 2:
-        console.clear();
+        console.clear();  //Se encarga de clear la cuenta.
         await crearCuenta();
         await main();
         break;
