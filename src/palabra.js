3370c41dfdb9c8c21ca615e47fa108e89423ba41
sebcalle <sebastianmeza@usantotomas.edu.co>
2022-03-10 21:56:15 -0500

    Se comenta las funciones de palabra.js

--- src/palabra.js
+++ src/palabra.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const ruta = path.join(__dirname, '../json/palabras.json');
 
+//Funcion que se encarga de cargar todas las palabras
 async function cargarPalabras() {
     if(fs.existsSync(ruta)){
         //Abrir el archivo 
@@ -15,6 +16,7 @@ async function cargarPalabras() {
     }
 }
 
+//Funcion que se encarga de verificar si la palabra que digito el usuario existe en el json y devuelve un boolean
 async function verificarPalabraExistente(palabra) {
     let datos = await cargarPalabras();
     let x = false;
@@ -27,8 +29,9 @@ async function verificarPalabraExistente(palabra) {
     return x;
 }
 
+//Función que se encarga de generar valor aleatorio y devolver la palabra del archivo palabras.json
 async function palabraAleatoria() {
-    let aleatorio = Math.round(Math.random() * 1000);
+    let aleatorio = Math.round(Math.random() * 1000);   //Se elige entre las primeras 1000 palabras
     let datos = await cargarPalabras();
     let palabra = datos.palabras[aleatorio];
     console.log(palabra);
